a578487187ec4a1f00410a083b17d7e9f49a29aa
Alejaldo <statfluct@yahoo.com>
2021-04-24 12:14:32 +0500

    Modify parallax

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,11 @@
 (function ($) {
     $('.parallax-1').paroller({
         factor: -0.3,
-        type: 'foreground',
-        transition: 'transform 0.6s ease',
+        transition: 'transform 0.2s ease',
         direction: 'horizontal'
     });
     $('.parallax-2').paroller({
         factor: 0.2,
-        type: 'foreground',
         transition: 'transform 0.2s ease',
         direction: 'horizontal'
     });
